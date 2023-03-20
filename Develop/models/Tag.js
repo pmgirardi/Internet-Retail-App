dab311cf24956bcdb99507f95ec83e6b525b04c7
pmgirardi <117805446+pmgirardi@users.noreply.github.com>
2023-03-20 10:14:26 -0700

    added fields and rules for Tag model

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -6,7 +6,21 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+  // define columns
+    id: {
+  // Integer.
+      type: DataTypes.INTEGER,
+  // Doesn't allow null values.
+      allowNull: false,
+  // Set as primary key.
+      primaryKey: true,
+  // Uses auto increment.
+      autoIncrement: true
+    },
+    tag_name: {
+  // String.
+      type: DataTypes.STRING
+    },
   },
   {
     sequelize,
