820449d94025099dc9cc48e9589c8b50e810843e
pmgirardi <117805446+pmgirardi@users.noreply.github.com>
2023-03-20 10:07:24 -0700

    added fields and rules for Category model

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,6 +7,22 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+    // Integer.
+      type: DataTypes.INTEGER,
+    // Doesn't allow null values.
+      allowNull: false,
+    // Set as primary key.
+      primaryKey: true,
+    // Uses auto increment.
+      autoIncrement: true
+  },
+    category_name: {
+  // String.
+      type: DataTypes.STRING,
+  // Doesn't allow null values.
+      allowNull: false
+    },
   },
   {
     sequelize,
