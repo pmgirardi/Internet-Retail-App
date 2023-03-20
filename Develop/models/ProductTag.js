90b814f0e02ee94ebbc90a24aea6325bed12d521
pmgirardi <117805446+pmgirardi@users.noreply.github.com>
2023-03-20 10:25:03 -0700

    added fields and rules for ProductTag model

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,38 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+  // define columns
+    id: {
+  // Integer.
+      type: DataTypes.INTEGER,
+  // Doesn't allow null values.
+      allowNull: false,
+  // Set as primary key.
+      primaryKey: true,
+  // Uses auto increment.
+      autoIncrement: true
+
+    },
+      product_id: {
+  // Integer.
+        type: DataTypes.INTEGER,
+  // References the `Product` model's `id`.
+        references: {
+          model: 'Product',
+          key: 'id',
+          unique: false
+        },
+      },
+      tag_id: {
+  // Integer.
+        type: DataTypes.INTEGER,
+  // References the `Tag` model's `id`.
+        references: {
+          model: 'Tag',
+          key: 'id',
+          unique: false
+        },
+      },
   },
   {
     sequelize,
